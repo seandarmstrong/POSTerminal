800cc86538c1d6fd1c64f9d9d951b066a75c980d
Bradley Freestone <bradley.freestone@gmail.com>
2018-08-02 15:47:08 -0400

    Made main menu. Making options actually do things

--- RadioShackPOS/POS.Library/Menu.cs
+++ RadioShackPOS/POS.Library/Menu.cs
@@ -1,15 +1,49 @@
-﻿namespace POS.Library
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POS.Library;
+using System.IO;
+
+namespace POS.Library
 {
     public class Menu
     {
-        public static void DisplayCategoryMenu()
+        public static void DisplayMainMenu()
         {
+            
+            var menu = String.Format("{0}\n{1}\n{2}\n{3}",
+                "What would you like to do?",
+                "1. View wares",
+                "2. Check out",
+                "3. Leave store");
+            Console.WriteLine(menu);
+            var userInput = int.Parse(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 1:
+                    DisplayProductMenu();
+                    break;
+
+            }
 
         }
 
         public static void DisplayProductMenu()
         {
-
+            StreamReader readProducts = new StreamReader(@"C: \Users\frees\source\repos\POSTerminal\RadioShackPOS\POS.Library\products.txt");
+            while (true)
+            {
+                string line = readProducts.ReadLine();
+                if (line == null)
+                    {
+                    break;
+                    }
+                Console.WriteLine(line);
+                Console.ReadLine();
+            }
         }
 
         public static void DisplayPaymentMethods()
--- RadioShackPOS/RadioShackPOS/Program.cs
+++ RadioShackPOS/RadioShackPOS/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace RadioShackPOS
+﻿using System;
+using POS.Library;
+
+namespace RadioShackPOS
 {
     class Program
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("Welcome to Radioshack");
+            Menu.DisplayMainMenu();
+            
         }
     }
 }
