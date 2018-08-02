d7a2bcb5aa239343c717da7c627317e00d454725
Mozzey <mbcacciano@gmail.com>
2018-08-02 15:45:39 -0400

    test these regexes, did ccNum, expDate, cvv, checkNum

--- RadioShackPOS/RadioShackPOS/Program.cs
+++ RadioShackPOS/RadioShackPOS/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace RadioShackPOS
+﻿using System;
+using POS.Library;
+using System.Text.RegularExpressions;
+
+namespace RadioShackPOS
 {
     class Program
     {
         static void Main(string[] args)
         {
+            System.Console.WriteLine("put in some test input");
+            var answer = Console.ReadLine();
+            var validator = new Validator();
+            if (validator.ValidExpDate(answer))
+            {
+                Console.WriteLine("valid");
+            }
+
+
+
+            Console.ReadKey();
         }
     }
 }
