6eb3cc3b7f2d42cb6264f442ad6978d06572ca4a
sean <armstrong.d.sean@gmail.com>
2018-08-02 21:30:44 -0400

    moved list build to product class and added return methods to use private properties of Product class.

--- RadioShackPOS/POS.Library/Order.cs
+++ RadioShackPOS/POS.Library/Order.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace POS.Library
+﻿namespace POS.Library
 {
     public class Order
     {
@@ -9,22 +6,6 @@ namespace POS.Library
         private float _salesTax = .06f;
         private float _grandTotal { get; set; }
 
-        public List<Product> orderList = new List<Product>();
 
-        public List<Product> BuildList()
-        {
-            string[] str = System.IO.File.ReadAllText(@"C:\Users\armst\Documents\Grand_Circus\POS_Terminal\POSTerminal\RadioShackPOS\POS.Library\products.txt")
-                .Split(new string[] { Environment.NewLine },
-                    StringSplitOptions.None);
-
-            for (int i = 0; i < str.Length; i++)
-            {
-                string[] s = str[i].Split(',');
-
-                orderList.Add(new Product(s[0], s[1], Convert.ToSingle(s[2]), s[3]));
-            }
-
-            return orderList;
-        }
     }
 }
--- RadioShackPOS/POS.Library/Product/Product.cs
+++ RadioShackPOS/POS.Library/Product/Product.cs
@@ -1,4 +1,7 @@
-﻿namespace POS.Library
+﻿using System;
+using System.Collections.Generic;
+
+namespace POS.Library
 {
     public class Product
     {
@@ -10,9 +13,47 @@
             _description = description;
         }
 
-        public string _category { get; set; }
-        public string _name { get; set; }
-        public float _price { get; set; }
-        public string _description { get; set; }
+        private string _category { get; set; }
+        private string _name { get; set; }
+        private float _price { get; set; }
+        private string _description { get; set; }
+
+        public List<Product> orderList = new List<Product>();
+
+        public List<Product> BuildList()
+        {
+            string[] str = System.IO.File.ReadAllText(@"C:\Users\armst\Documents\Grand_Circus\POS_Terminal\POSTerminal\RadioShackPOS\POS.Library\products.txt")
+                .Split(new string[] { Environment.NewLine },
+                    StringSplitOptions.None);
+
+            foreach (var t in str)
+            {
+                string[] s = t.Split(',');
+
+                orderList.Add(new Product(s[0], s[1], Convert.ToSingle(s[2]), s[3]));
+            }
+
+            return orderList;
+        }
+
+        public string ReturnCategory()
+        {
+            return _category;
+        }
+
+        public string ReturnName()
+        {
+            return _name;
+        }
+
+        public float ReturnPrice()
+        {
+            return _price;
+        }
+
+        public string ReturnDescription()
+        {
+            return _description;
+        }
     }
 }
