461ed7fbcc32a664533945772aed8e9cff06f520
Mozzey <mbcacciano@gmail.com>
2018-08-02 21:27:51 -0400

    had to commit some changes before i could pull

--- RadioShackPOS/RadioShackPOS/Program.cs
+++ RadioShackPOS/RadioShackPOS/Program.cs
@@ -10,17 +10,8 @@ namespace RadioShackPOS
         {
             Console.ForegroundColor = ConsoleColor.White;
             ////////// TESTING - MIKE ///////////////////////
-            //System.Console.WriteLine("put in some test input");
-            //var answer = Console.ReadLine();
-            var validator = new Validator();
             var transaction = new POS.Library.Transaction.Transaction();
-            //var change = transaction.CashTransaction();
             transaction.CCTransaction();
-            
-            //if (validator.ValidCVV(answer))
-            //{
-            //    Console.WriteLine("valid");
-            //}
             ///////////////////////////////////////////////
 
 
