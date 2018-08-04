1c96be81c9125a7ec8d055839bc49e5930897919
Mozzey <mbcacciano@gmail.com>
2018-08-03 20:54:47 -0400

    smashed save all key just in case

--- RadioShackPOS/RadioShackPOS/Program.cs
+++ RadioShackPOS/RadioShackPOS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using POS.Library;
+using POS.Library.Transactions;
 using System;
 
 namespace RadioShackPOS
@@ -7,31 +8,27 @@ namespace RadioShackPOS
     {
         static void Main(string[] args)
         {
-            //System.Console.WriteLine("Welcome to Radioshack");
-            //Menu.DisplayMainMenu();
-            //System.Console.WriteLine("put in some test input");
-            //var answer = Console.ReadLine();
-            //var validator = new Validator();
-            //if (validator.ValidExpDate(answer))
-            //{
-            //    Console.WriteLine("valid");
-            //}
-            //Console.ReadKey();
+            System.Console.WriteLine("Welcome to Radioshack");
+            Menu.DisplayMainMenu();
+            System.Console.WriteLine("put in some test input");
+            var answer = Console.ReadLine();
+            var validator = new Validator();
+            if (validator.ValidExpDate(answer))
+            {
+                Console.WriteLine("valid");
+            }
             //Console.ForegroundColor = ConsoleColor.White;
             ////////// TESTING - MIKE ///////////////////////////
-            // CASH METHOD IS HERE IF YOU WANT TO TEST IT
-            float total = 100.00f;
-            var cash = new Cash(total); ;
-            cash.Transaction(total);
-            //Console.WriteLine(cashTransaction);
+            // CASH METHOD IS HERE IF YOU WANT TO TEST IT - hard coded total for testing
+            //float total = 100.00f;
+            //var cash = new Cash(total);
+            //cash.Transaction(total);
             //CREDIT CARD METHOD IS HERE IF YOU WANT TO TEST IT
-            var creditCard = new CreditCard(total);
-            creditCard.Transaction(total);
-            //Console.WriteLine(ccTransaction);
+            //var creditCard = new CreditCard(total);
+            //creditCard.Transaction(total);
             //CHECK METHOD IS HERE IF YOU WANT TO TEST IT
-            var check = new Check(total);
-            check.Transaction(total);
-            //Console.WriteLine(checkTransaction);
+            //var check = new Check(total);
+            //check.Transaction(total);
             Console.ReadKey();
             /////////////// END TESTING //////////////////////////
 
