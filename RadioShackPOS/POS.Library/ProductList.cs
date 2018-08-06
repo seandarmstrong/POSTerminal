385fdd8795c85b432bb6f829026376de27a7bba8
sean <armstrong.d.sean@gmail.com>
2018-08-06 13:31:31 -0400

    will not allow 0 quantity to be entered for products now

--- RadioShackPOS/POS.Library/ProductList.cs
+++ RadioShackPOS/POS.Library/ProductList.cs
@@ -24,7 +24,7 @@ namespace POS.Library
             string[] fields;
             try
             {
-                using (TextFieldParser parser = new TextFieldParser(mikePath))
+                using (TextFieldParser parser = new TextFieldParser(seanPath))
                 {
                     parser.TextFieldType = FieldType.Delimited;
                     parser.SetDelimiters(",");
